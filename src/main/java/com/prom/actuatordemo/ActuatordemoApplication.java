4f9cd096575b02b9b279332c9486df12a8e7d67a
huangbiao <h295203236@163.com>
2020-07-04 07:01:26 +0800

    g

--- src/main/java/com/prom/actuatordemo/ActuatordemoApplication.java
+++ src/main/java/com/prom/actuatordemo/ActuatordemoApplication.java
@@ -3,6 +3,7 @@ package com.prom.actuatordemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// rebase-branch change13
 @SpringBootApplication
 public class ActuatordemoApplication {
 

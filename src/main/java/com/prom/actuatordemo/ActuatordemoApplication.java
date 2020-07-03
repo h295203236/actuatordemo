430d617765443e3227e5cd345df52f3b9f8d9b1a
huangbiao <h295203236@163.com>
2020-07-04 07:00:26 +0800

    f

--- src/main/java/com/prom/actuatordemo/ActuatordemoApplication.java
+++ src/main/java/com/prom/actuatordemo/ActuatordemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // master change11
+// master change12
 @SpringBootApplication
 public class ActuatordemoApplication {
 
